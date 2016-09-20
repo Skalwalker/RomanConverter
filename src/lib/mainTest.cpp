5e01924192e7b033ae7cc8cb7c0e178fc8a589bb
Renato Nobre <rekanobre@gmail.com>
2016-09-20 14:06:39 -0300

    Passed Test #3 Variants

--- src/lib/mainTest.cpp
+++ src/lib/mainTest.cpp
@@ -7,6 +7,9 @@ TEST(Conversion, Single){
 	EXPECT_EQ(1, romanToArabic((char*)"I"));
 	EXPECT_EQ(5, romanToArabic((char*)"V"));
 	EXPECT_EQ(-1, romanToArabic((char*)"!"));
+	EXPECT_EQ(-1, romanToArabic((char*)"U"));
+	EXPECT_EQ(-1, romanToArabic((char*)"1"));
+	EXPECT_EQ(-1, romanToArabic((char*)"i"));
 
 }
 
